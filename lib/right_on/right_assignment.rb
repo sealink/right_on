f70e749910da4af2e8dc1205db75a1323edbd4d6
Michael Noack <michael@noack.com.au>
2014-07-23 12:19:30 +0930

    Specify primary_key so can do rights_roles as has many through

--- lib/right_on/right_assignment.rb
+++ lib/right_on/right_assignment.rb
@@ -2,6 +2,7 @@ require 'active_record'
 
 class RightAssignment < ActiveRecord::Base
   self.table_name = :rights_roles
+  self.primary_key = [:right_id, :role_id]
 
   belongs_to :right
   belongs_to :role
