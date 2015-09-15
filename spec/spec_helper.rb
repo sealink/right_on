84f4d9fc8af776d38bb973f3b6ecb3efc8f6d27a
Michael Noack <michael@noack.com.au>
2015-09-15 11:49:50 +0930

    Extract coverage and increase 2%

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,33 +9,7 @@ require 'rubygems'
 require 'bundler/setup'
 
 require 'support/bootstrap'
-
-MINIMUM_COVERAGE = 63
-
-unless ENV['COVERAGE'] == 'off'
-  require 'simplecov'
-  require 'simplecov-rcov'
-  require 'coveralls'
-  Coveralls.wear!
-
-  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
-    SimpleCov::Formatter::RcovFormatter,
-    Coveralls::SimpleCov::Formatter
-  ]
-  SimpleCov.start do
-    add_filter '/vendor/'
-    add_filter '/spec/'
-    add_group 'lib', 'lib'
-  end
-  SimpleCov.at_exit do
-    SimpleCov.result.format!
-    percent = SimpleCov.result.covered_percent
-    unless percent >= MINIMUM_COVERAGE
-      puts "Coverage must be above #{MINIMUM_COVERAGE}%. It is #{"%.2f" % percent}%"
-      Kernel.exit(1)
-    end
-  end
-end
+require 'support/coverage'
 
 require 'right_on'
 require 'right_on/rails'
