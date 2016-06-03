1281ac52bcdcea23c9a45ac1e18f66066ffab38c
Alessandro Berardi <berardialessandro@gmail.com>
2016-06-03 11:13:39 +0930

    Ensures every defined class is under the RightOn module

--- lib/right_on/restricted_by_right.rb
+++ lib/right_on/restricted_by_right.rb
@@ -1,52 +1,53 @@
-module RestrictedByRight
+module RightOn
+  module RestrictedByRight
 
-  def self.included(base)
-    base.extend(ClassMethods)
-  end
+    def self.included(base)
+      base.extend(ClassMethods)
+    end
 
-  module ClassMethods
-    def restricted_by_right(options = {})
-      options ||= {}
-      options[:group] ||= 'other'
-      Right.associate_group(self, options[:group])
+    module ClassMethods
+      def restricted_by_right(options = {})
+        options ||= {}
+        options[:group] ||= 'other'
+        Right.associate_group(self, options[:group])
 
-      class << self
-        def accessible_to(user)
-          all.select{|o| user.rights.include?(o.right)}
+        class << self
+          def accessible_to(user)
+            all.select{|o| user.rights.include?(o.right)}
+          end
         end
+
+        include InstanceMethods
+
+        belongs_to :right, :class_name => 'RightOn::Right'
+        before_create :create_access_right!
+        after_destroy :destroy_access_right!
       end
 
-      include InstanceMethods
+    end
 
-      belongs_to :right
-      before_create :create_access_right!
-      after_destroy :destroy_access_right!
+    module InstanceMethods
+
+      private
+
+        def create_access_right!
+          right_name = "#{self.class.name.titleize}: #{name}"
+          self.right = find_right(right_name) || Right.create!(:name => right_name)
+        end
+
+        def find_right(name)
+          if Right.respond_to? :find_by
+            Right.find_by(:name => name)
+          else
+            Right.find_by_name(name)
+          end
+        end
+
+        def destroy_access_right!
+          self.right.try(:destroy)
+        end
+
     end
 
   end
-
-  module InstanceMethods
-
-    private
-
-      def create_access_right!
-        right_name = "#{self.class.name.titleize}: #{name}"
-        self.right = find_right(right_name) || Right.create!(:name => right_name)
-      end
-
-      def find_right(name)
-        if Right.respond_to? :find_by
-          Right.find_by(:name => name)
-        else
-          Right.find_by_name(name)
-        end
-      end
-
-      def destroy_access_right!
-        self.right.try(:destroy)
-      end
-
-  end
-
 end
-
--- lib/right_on/right.rb
+++ lib/right_on/right.rb
@@ -1,176 +1,179 @@
 require 'active_record'
-class Right < ActiveRecord::Base
 
-  has_and_belongs_to_many :roles
+module RightOn
+  class Right < ActiveRecord::Base
 
-  validates_presence_of :name
-  validates_uniqueness_of :name
+    has_and_belongs_to_many :roles, :class_name => 'RightOn::Role'
 
-  if ::Rails.version > '3'
-    scope :ordered, -> { order :name }
-  else
-    named_scope :ordered, :order => 'name'
-  end
+    validates_presence_of :name
+    validates_uniqueness_of :name
 
-  after_save :clear_cache
-  after_destroy :clear_cache
-
-  attr_accessor :group
-
-  class << self
-    @@restricted_by_right_groups = {}
-
-    def associate_group(klass, group)
-      @@restricted_by_right_groups[klass] = group
-      has_one klass.table_name.singularize.to_sym, :dependent => :restrict
+    if ::Rails.version > '3'
+      scope :ordered, -> { order :name }
+    else
+      named_scope :ordered, :order => 'name'
     end
 
-    def associated_groups
-      @@restricted_by_right_groups
-    end
+    after_save :clear_cache
+    after_destroy :clear_cache
 
-    def rights_yaml(file_path)
-      @@rights_yaml = file_path
-    end
+    attr_accessor :group
 
-    def by_groups
-      rights = []
-      rights += regular_rights_with_group
-      rights += restricted_rights_with_group
-      rights.group_by(&:group)
-    end
+    class << self
+      @@restricted_by_right_groups = {}
 
-    def regular_rights_with_group
-      yaml = YAML::load_file(@@rights_yaml)
-      rights = []
-      rights_by_name = Hash[Right.all.map{|r| [r.name, r]}]
-      yaml['rights'].each_pair do |group, right_names|
-        rights_for_group = []
-        right_names.each do |right_name|
-          if right_name.is_a?(String) # controller
-            r = rights_by_name[right_name]
-            raise right_name if r.nil?
-            rights_for_group << r
-          else right_name.is_a?(Hash) # controller + actions
-            controller, actions = right_name.first
-            r = rights_by_name[controller]
-            if r
+      def associate_group(klass, group)
+        @@restricted_by_right_groups[klass] = group
+        has_one klass.table_name.singularize.to_sym, :dependent => :restrict
+      end
+
+      def associated_groups
+        @@restricted_by_right_groups
+      end
+
+      def rights_yaml(file_path)
+        @@rights_yaml = file_path
+      end
+
+      def by_groups
+        rights = []
+        rights += regular_rights_with_group
+        rights += restricted_rights_with_group
+        rights.group_by(&:group)
+      end
+
+      def regular_rights_with_group
+        yaml = YAML::load_file(@@rights_yaml)
+        rights = []
+        rights_by_name = Hash[Right.all.map{|r| [r.name, r]}]
+        yaml['rights'].each_pair do |group, right_names|
+          rights_for_group = []
+          right_names.each do |right_name|
+            if right_name.is_a?(String) # controller
+              r = rights_by_name[right_name]
+              raise right_name if r.nil?
               rights_for_group << r
-            end
-            actions.each do |action|
-              name = "#{controller}##{action}"
-              r = rights_by_name[name]
-              raise name.inspect + "****" + right_name.inspect + '---' + action_right.inspect if r.nil?
-              rights_for_group << r
+            else right_name.is_a?(Hash) # controller + actions
+              controller, actions = right_name.first
+              r = rights_by_name[controller]
+              if r
+                rights_for_group << r
+              end
+              actions.each do |action|
+                name = "#{controller}##{action}"
+                r = rights_by_name[name]
+                raise name.inspect + "****" + right_name.inspect + '---' + action_right.inspect if r.nil?
+                rights_for_group << r
+              end
             end
           end
+          rights_for_group.each{|r| r.group = group}
+          rights += rights_for_group
         end
-        rights_for_group.each{|r| r.group = group}
-        rights += rights_for_group
+        rights
       end
-      rights
+
+      def restricted_rights_with_group
+        rights = []
+        @@restricted_by_right_groups.each_pair do |klass, group|
+          rights += all_rights(klass).map(&:right).each do |right|
+            right.group = group
+          end
+        end
+        rights
+      end
+
+      def all_rights(klass)
+        if klass.respond_to? :includes
+          klass.includes(:right).all
+        else
+          klass.all(:include => :right)
+        end
+      end
     end
 
-    def restricted_rights_with_group
-      rights = []
-      @@restricted_by_right_groups.each_pair do |klass, group|
-        rights += all_rights(klass).map(&:right).each do |right|
-          right.group = group
-        end
-      end
-      rights
-    end
+    # Is this right allowed for the given context?
+    #
+    # Context params is an option hash:
+    #   :controller => controller name
+    #   :action     => action name
+    #
+    # The context tells us the state of the request being made.
 
-    def all_rights(klass)
-      if klass.respond_to? :includes
-        klass.includes(:right).all
+    def allowed?(context={})
+      return false unless controller == context[:controller]
+      if action
+        action_permitted?(context[:action])
       else
-        klass.all(:include => :right)
+        # right without action works if no specific right exists
+        # e.g. can't edit if there's a edit or change right defined
+        # as you must used that specific right
+        specific_rights = Array(APPLICABLE_RIGHTS[context[:action].to_sym]) + [context[:action]]
+        specific_rights.all?{|action| Right["#{context[:controller]}##{action}"].nil?}
       end
     end
-  end
 
-  # Is this right allowed for the given context?
-  #
-  # Context params is an option hash:
-  #   :controller => controller name
-  #   :action     => action name
-  #
-  # The context tells us the state of the request being made.
+    APPLICABLE_RIGHTS = {
+      :new     => [:change],
+      :edit    => [:change],
+      :update  => [:change],
+      :create  => [:change],
+      :destroy => [:change],
+      :index   => [:change, :view],
+      :show    => [:change, :view]
+    }
 
-  def allowed?(context={})
-    return false unless controller == context[:controller]
-    if action
-      action_permitted?(context[:action])
-    else
-      # right without action works if no specific right exists
-      # e.g. can't edit if there's a edit or change right defined
-      # as you must used that specific right
-      specific_rights = Array(APPLICABLE_RIGHTS[context[:action].to_sym]) + [context[:action]]
-      specific_rights.all?{|action| Right["#{context[:controller]}##{action}"].nil?}
+    CHANGE_ACTIONS = %w(new edit update create destroy index show)
+
+    VIEW_ACTIONS = %w(index show)
+
+    def action_permitted?(context_action)
+      case action.to_sym
+      when :change
+        CHANGE_ACTIONS.include?(context_action)
+      when :view
+        VIEW_ACTIONS.include?(context_action)
+      else
+        action == context_action
+      end
     end
-  end
 
-  APPLICABLE_RIGHTS = {
-    :new     => [:change],
-    :edit    => [:change],
-    :update  => [:change],
-    :create  => [:change],
-    :destroy => [:change],
-    :index   => [:change, :view],
-    :show    => [:change, :view]
-  }
-
-  CHANGE_ACTIONS = %w(new edit update create destroy index show)
-
-  VIEW_ACTIONS = %w(index show)
-
-  def action_permitted?(context_action)
-    case action.to_sym
-    when :change
-      CHANGE_ACTIONS.include?(context_action)
-    when :view
-      VIEW_ACTIONS.include?(context_action)
-    else
-      action == context_action
+    def sensible_name
+      name.humanize.titleize.gsub(/#/, ' - ')
     end
-  end
 
-  def sensible_name
-    name.humanize.titleize.gsub(/#/, ' - ')
-  end
+    def to_s
+      name
+    end
 
-  def to_s
-    name
-  end
+    def self.cache
+      @@cache ||= Rails.cache
+    end
 
-  def self.cache
-    @@cache ||= Rails.cache
-  end
+    def self.cache=(cache)
+      @@cache = cache
+    end
 
-  def self.cache=(cache)
-    @@cache = cache
-  end
+    def self.clear_cache
+      cache.delete('Right.all')
+    end
 
-  def self.clear_cache
-    cache.delete('Right.all')
-  end
+    def clear_cache
+      self.class.clear_cache
+    end
 
-  def clear_cache
-    self.class.clear_cache
-  end
+    attr_accessor :rights
+    def self.[](name)
+      @rights = cache.read('Right.all') || calculate_and_write_cache
+      @rights[name]
+    end
 
-  attr_accessor :rights
-  def self.[](name)
-    @rights = cache.read('Right.all') || calculate_and_write_cache
-    @rights[name]
-  end
+    private
+    def self.calculate_and_write_cache
+      right_cache = Hash[Right.all.map{|r|[r.name, r.id]}]
+      cache.write('Right.all', right_cache) or raise RuntimeError, "Could not cache rights"
+      right_cache
+    end
 
-  private
-  def self.calculate_and_write_cache
-    right_cache = Hash[Right.all.map{|r|[r.name, r.id]}]
-    cache.write('Right.all', right_cache) or raise RuntimeError, "Could not cache rights"
-    right_cache
   end
-
 end
